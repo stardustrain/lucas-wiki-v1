221a1811ba5adbaaddf7ff48abb6c6131c740aa3
stardustrain <kthanterran@gmail.com>
2018-12-13 18:49:07 +0900

    Add drawing bar chart WIP.

--- docs/.vuepress/components/Disqus.vue
+++ docs/.vuepress/components/Disqus.vue
@@ -5,12 +5,11 @@
 <script>
   export default {
     mounted() {
-      console.log('test')
       const disqus_config = function () {
         this.page.url = window.location.origin
         this.page.identifier = window.location.pathname
       }
-      (function() { // DON'T EDIT BELOW THIS LINE
+      (function() {
         const d = document, s = d.createElement('script')
         s.src = 'https://wiki-lucashan-space.disqus.com/embed.js'
         s.setAttribute('data-timestamp', +new Date())
