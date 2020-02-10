7101c540ae9713c5bbbb69544b655779cf0cad8d
stardustrain <kthanterran@gmail.com>
2020-02-11 00:38:09 +0900

    resolve #7 Update vuepress version

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,17 +1,34 @@
 <template>
   <ParentLayout>
-    <template #page-bottom>
-      <vue-disqus />
+    <template v-if="!$page.frontmatter.disableDisqus" #page-bottom>
+      <Disqus />
     </template>
   </ParentLayout>
 </template>
 
 <script>
-  import ParentLayout from '@parent-theme/layouts/Layout'
+import ParentLayout from "@parent-theme/layouts/Layout";
 
-  export default {
-    components: {
-      ParentLayout,
-    },
+export default {
+  components: {
+    ParentLayout
+  },
+  watch: {
+    $route(to, from) {
+      if (to.path !== from.path) {
+        if (typeof window !== "undefined" && window.DISQUS) {
+          window.DISQUS.reset({ reload: true });
+        }
+      }
+    }
   }
+};
 </script>
+
+<style lang="css" scoped>
+#disqus_thread {
+  max-width: 740px;
+  margin: 0 auto;
+  padding: 2rem 2.5rem;
+}
+</style>
